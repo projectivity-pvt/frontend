c89f67aa10c519be33b7fd24b288fd5b67b4e436
Deevoid <dev.4.pandey@gmail.com>
2021-11-05 01:00:37 +0530

    add jit mode, tailwind plugins and base typography layers

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div tw="bg-indigo-500">Welcome</div>;
+  return (
+    <>
+      <div
+        className="custom"
+        tw="bg-indigo-500 text-red-500 select-all text-xl "
+      >
+        Welcome
+      </div>
+      <h1>h1 tag</h1>
+      <h2>h2 tag</h2>
+      <button tw="btn">Button</button>
+    </>
+  );
 };
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
+  mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
@@ -7,5 +10,33 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: "class",
+    }),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/typography"),
+    // add base styles here
+    plugin(function ({ addBase, theme }) {
+      addBase({
+        h1: { fontSize: theme("fontSize.2xl") },
+        h2: { fontSize: theme("fontSize.xl") },
+        h3: { fontSize: theme("fontSize.lg") },
+        h4: { fontSize: theme("fontSize.base") },
+        h5: { fontSize: theme("fontSize.sm") },
+        h6: { fontSize: theme("fontSize.xs") },
+      });
+    }),
+    // add components here
+    // plugin(function ({ addComponents, theme }) {
+    //   const buttons = {
+    //     ".btn": {
+    //       padding: `${theme("spacing.2")} ${theme("spacing.4")}`,
+    //       borderRadius: theme("borderRadius.md"),
+    //       fontWeight: theme("fontWeight.600"),
+    //     },
+    //   };
+    //   addComponents(buttons);
+    // }),
+  ],
 };
