450f946c35ecaba75fff50f83cbdf172fea8549f
Deevoid <dev.4.pandey@gmail.com>
2021-11-04 16:47:30 +0530

    add styled components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import styled from "styled-components";
+
+const Para = styled.div`
+  color: red;
+`;
 
 const Home: NextPage = () => {
-  return <div className="bg-indigo-500">Welcome</div>;
+  return (
+    <div className="bg-indigo-500">
+      Welcome
+      <Para>asdf</Para>
+    </div>
+  );
 };
 
 export default Home;
