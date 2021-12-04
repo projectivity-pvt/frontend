7d017554b230bee1e8e84cc3848b923601cb11ac
Dev Pandey <dev.4.pandey@gmail.com>
2021-12-04 11:11:28 +0530

    vendor project list item responsive

--- components/organisms/VendorProjectListItem/VendorProjectListItem.tsx
+++ components/organisms/VendorProjectListItem/VendorProjectListItem.tsx
@@ -6,12 +6,14 @@ interface Props {}
 
 export const VendorProjectListItem = (props: Props) => (
   <div className="bg-cool-gray-100 p-3 rounded-lg flex items-center">
-    <div className="avatar rounded-full bg-primary-600 text-white flex items-center justify-center h-14 w-14 mr-5">
+    <div className="avatar rounded-full bg-primary-600 text-white flex items-center justify-center h-10 w-10 md:h-14 md:w-14 mr-2 md:mr-5">
       A
     </div>
     <div>
-      <h3 className="font-semibold">Project Name Burger Singh</h3>
-      <p className="flex items-center gap-5 text-gray-600">
+      <h3 className="font-semibold text-sm md:text-lg truncate max-w-[160px] md:max-w-full">
+        Project Name Burger Singh
+      </h3>
+      <p className="flex items-center gap-5 text-gray-600 text-sm md:text-base">
         <span>Dharma Productions</span>
         <span className="md:flex items-center gap-2 hidden">
           <ClockIcon height={16} width={16} />
@@ -21,10 +23,10 @@ export const VendorProjectListItem = (props: Props) => (
     </div>
     <div className="ml-auto text-right">
       <p>
-        <span className="font-medium mr-1">Income:</span>
+        <span className="font-medium mr-1 hidden md:inline-block">Income:</span>
         <span className="font-bold text-lg">$196</span>
       </p>
-      <Badge className="bg-blue-500 text-white">In Progress</Badge>
+      <Badge className="bg-blue-500 text-white truncate">In Progress</Badge>
     </div>
   </div>
 )
